41eacec341a003a33c8fff5ae7bd75f7d2c0f3c3
Vyshnavi Paka <vyshnavipaka2010@gmail.com>
2025-12-19 16:27:48 +0530

    chore: Add welcome message with current date

--- Program.cs
+++ Program.cs
@@ -23,8 +23,16 @@ namespace PatientAppointmentSystem
             bool running = true;
 
             while (running)
+
             {
+
+
+
                 Console.Clear();
+                Console.WriteLine("Welcome to HCA Healthcare Patient Appointment System");
+                Console.WriteLine("Current date: " + DateTime.Today.ToLongDateString());
+                Console.WriteLine();
+
                 Console.WriteLine("=== Patient Appointment System (HCA Healthcare Demo) ===");
                 Console.WriteLine("1. Add Appointment");
                 Console.WriteLine("2. View All Appointments");
